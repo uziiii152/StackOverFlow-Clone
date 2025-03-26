17caf54346d5add78b1e03e9a6d5b32a714b24bf
uziiii152 <uzairqaseem162@gmail.com>
2025-03-26 20:59:15 +0500

    zustand persist and immer with store integration login create logout

--- stackoverflow-appwrite/src/Store/Auth.ts
+++ stackoverflow-appwrite/src/Store/Auth.ts
@@ -1,3 +1,13 @@
+/*
+ model on which zustand works with the nextjs
+first import the necessary packages
+2_ Create the interface for the store
+3_ Create the store with the help of persist and immer
+4_ Store state and methods
+5_ Rehydrate the store 
+
+*/
+
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 import { persist } from "zustand/middleware";
@@ -10,17 +20,30 @@ import { AppwriteException,ID, Models } from "appwrite";
     reputation:number 
  }
 
- interface AuthStore {
+ interface IAuthStore {
     session: Models.Session | null,
     jwt: string | null,
     user: Models.User<UserPrefs> | null,
     hydrated: boolean,
     setHydrated():void,
     verifySession():Promise<void>,
-    login(email:string,password:string):Promise<{success:boolean;
+    login(
+        email:string,
+        password:string
+    ):Promise<{
+        success:boolean;
         error?:AppwriteException
     }>,
-    logout():promise<void>
+    createAccount(
+        name:string,
+        email:string,
+        password:string
+    ):Promise<{
+        success:boolean;
+        error?:AppwriteException
+    }>,
+    logout():Promise<void>
+    
 }
 
 
@@ -31,16 +54,78 @@ export const useAuthStore = create<IAuthStore>()(
             jwt:null,
             user:null,
             hydrated:false,
+
+            setHydrated(){
+                set({hydrated:true})
+            },
+            async verifySession(){
+                try {
+                   const session = await account.getSession("current")
+                   set({session})
+        
+                } catch (error) {
+                    console.log(error)
+                }
+            },
+            
+            async login(email:string,password:string){
+                try {
+                  const session = await account.createEmailPasswordSession(email,password) 
+                  const [user,{jwt}] = await Promise.all([
+                    account.get<UserPrefs>(),
+                    account.createJWT()
+
+                  ])
+                  if (!user.prefs?.reputation) await account.updatePrefs<UserPrefs>({reputation:0})
+
+                    set({session,user,jwt})
+                    return {success:true}
+                } catch (error) {
+                   console.log(error) 
+                   return {success:false,error:error instanceof AppwriteException ? error : undefined}
+                }
+            },
+            async createAccount(name:string,email:string,password:string){
+                try {
+                    
+                    await account.create(ID.unique(),email,password,name)
+                    
+                    return {success:true}
+                  } catch (error) {
+                     console.log(error) 
+                     return {success:false,error:error instanceof AppwriteException ? error : undefined}
+                  }
+            },
+            async logout(){
+                try {
+                   await account.deleteSession("current")
+                   set({session:null,user:null,jwt:null}) 
+                } catch (error) {
+                    console.log(error)
+                }
+            }
         }) ),
         {
             name: 'auth',
             onRehydrateStorage(){
                 return (state,error) => {
-                    if(!error) state?.setHydrated(){
-                        set({hydrated:true})
-                    }
+                    if(!error) state?.setHydrated()
                 }
             }
         }
     )
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
